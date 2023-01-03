688a85706f3ef689c13174f1760fe57cee6631fc
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2023-01-03 19:35:35 +0800

    add webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,58 @@
 const path = require("path");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports ={
+module.exports = {
+    // context: path.resolve(__dirname, "src"), // базовый путь с проектом
     mode: "development", // режим разработки, js не сжимается.
-    entry: "./src/index.js",
+    entry: "./src/index.js", // точка входа
     output: {            // директория со сборкой проекта
         path: path.resolve(__dirname, "dist"),  // название директории
-        filename: "[name].[hash].js"            // название файлов js
+        filename: "[name].[hash].js",           // название файлов js
+        assetModuleFilename: path.join("images","[name].[contenthash][ext]") // путь для хранения assets
     },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            // sourceMap: true
+                        }
+                    },
+                    'postcss-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.svg$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: path.resolve(__dirname,"./src/static/[name].[contenthash][ext]"), // проверить!
+                }
+            }
 
-
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, "./src/index.html")
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+    devServer: {
+        watchFiles: path.resolve(__dirname, 'src'),
+        port: 3000,
+    },
 }
