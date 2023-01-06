4e73e40cab774e50718f45d652a21738e553ff0b
DmitriySotnikov <dmitrich.1983.38@gmail.com>
2023-01-06 22:15:47 +0800

    refactoring webpack config

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import "../styles/main.scss"
+import logo from "../static/img/webpack.png"
 
 function App({}) {
  return (
      <div>
          <h1>Главная страница будущего приложения</h1>
+         <img className="img" src={logo}/>
      </div>
  )
 }
